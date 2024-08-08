3e3a8798f70dd54f1a52ca2fe99d705f61ef93ee
Yücel YILMAZ <89340331+Yucel0@users.noreply.github.com>
2024-08-08 21:59:55 +0300

    mail-api
    mail api geliştirmesi tamamlandı

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import { config } from 'dotenv';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3001;
+
+config();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+function render(fullname, message, email) {
+    return `<div>
+        <p>Gönderici Maili: ${email}</p>
+        <p>Gönderici Adı Soyadı: ${fullname}</p>
+        <p>Gönderici Mesajı: ${message}</p>
+    </div>`;
+}
+
+app.post('/api/send-mail',(req,res) => {
+    const { fullname, email, subject, message } = req.body;
+
+    const transport = nodemailer.createTransport({
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASSWORD
+        },
+        port: 587,
+        host: "smtp.gmail.com",
+        service: "gmail"
+    });
+
+    transport.sendMail({
+        from: process.env.EMAIL,
+        to: 'yclylmzx@gmail.com',
+        subject,
+        html: render(fullname,message,email)
+    },(err,info) => {
+        if(err){
+            return res.json({
+                status: false,
+                message: err.message
+            })
+        }else {
+            return res.json({
+               status: true,
+               message: 'Mesajınız Gönderildi!' 
+            })
+        }
+    });
+});
+
+app.listen(PORT,() => {
+    console.log('sunucu çalışıyor!');
+})
